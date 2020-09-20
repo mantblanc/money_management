bd6ae73d2fd5143a9b4f2d92059696eed3d07d17
Moon Seong Ho <crow810@gmail.com>
2020-09-20 19:27:52 +0900

    money management project start

---
+++ money_list/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MoneyListConfig(AppConfig):
+    name = 'money_list'
---
+++ money_main/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MoneyMainConfig(AppConfig):
+    name = 'money_main'
